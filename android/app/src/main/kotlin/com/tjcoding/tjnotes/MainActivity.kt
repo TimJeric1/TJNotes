1802068e0023ca8a0bc8ce104ad6ef1289c5049e
timjeric1 <timjeric@hotmail.com>
2024-04-04 08:41:23 +0200

    renamed package name to TJNotes

---
+++ android/app/src/main/kotlin/com/tjcoding/tjnotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tjcoding.tjnotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/tjcoding/todoapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tjcoding.todoapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
